72bc65e3924924c1e62efd520fa320eba81598ae
YugandharSaiChowdary <yugandharsai2801@gmail.com>
2025-11-24 00:06:47 +0530

    setup

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup, find_packages
 
+def get_requirements(file_path: str) -> list[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = fileobj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
 
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+    return requirements
+    
 setup (
     name='ml_project',
     version='0.1',
