2275ca4ba1819f42a427ffe906cc865b023e2885
YugandharSaiChowdary <yugandharsai2801@gmail.com>
2026-01-08 22:37:50 +0530

    EDA

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 def get_requirements(file_path: str) -> list[str]:
     requirements=[]
     with open(file_path) as file_obj:
-        requirements = fileobj.readlines()
+        requirements = file_obj.readlines()
         requirements = [req.replace("\n","") for req in requirements]
 
         if '-e .' in requirements:
